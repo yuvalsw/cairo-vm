8f697d148c5a0f96499d8eca52d9c755d66ae135
Olivier Desenfans <olivier@moonsonglabs.com>
2023-12-14 11:16:35 +0100

    Execute task hint #2: load program (#24)
    The new `ProgramLoader` struct that takes a program and loads it
    into memory.

--- vm/src/hint_processor/builtin_hint_processor/bootloader/execute_task_hints.rs
+++ vm/src/hint_processor/builtin_hint_processor/bootloader/execute_task_hints.rs
@@ -1,10 +1,15 @@
 use std::collections::HashMap;
 
+use crate::hint_processor::builtin_hint_processor::bootloader::program_loader::ProgramLoader;
+use crate::hint_processor::builtin_hint_processor::bootloader::types::{BootloaderVersion, Task};
 use crate::hint_processor::builtin_hint_processor::bootloader::vars;
-use crate::hint_processor::builtin_hint_processor::hint_utils::insert_value_from_var_name;
+use crate::hint_processor::builtin_hint_processor::hint_utils::{
+    get_ptr_from_var_name, insert_value_from_var_name,
+};
 use crate::hint_processor::hint_processor_definition::HintReference;
 use crate::serde::deserialize_program::ApTracking;
 use crate::types::exec_scope::ExecutionScopes;
+use crate::types::relocatable::Relocatable;
 use crate::vm::errors::hint_errors::HintError;
 use crate::vm::vm_core::VirtualMachine;
 
@@ -30,9 +35,49 @@ pub fn allocate_program_data_segment(
     Ok(())
 }
 
+/// Implements
+///
+/// from starkware.cairo.bootloaders.simple_bootloader.utils import load_program
+///
+/// # Call load_program to load the program header and code to memory.
+/// program_address, program_data_size = load_program(
+///     task=task, memory=memory, program_header=ids.program_header,
+///     builtins_offset=ids.ProgramHeader.builtin_list)
+/// segments.finalize(program_data_base.segment_index, program_data_size)
+pub fn load_program_hint(
+    vm: &mut VirtualMachine,
+    exec_scopes: &mut ExecutionScopes,
+    ids_data: &HashMap<String, HintReference>,
+    ap_tracking: &ApTracking,
+) -> Result<(), HintError> {
+    let program_data_base: Relocatable = exec_scopes.get(vars::PROGRAM_DATA_BASE)?;
+    let task: Task = exec_scopes.get(vars::TASK)?;
+    let program = task.get_program();
+
+    let program_header_ptr = get_ptr_from_var_name("program_header", vm, ids_data, ap_tracking)?;
+
+    // Offset of the builtin_list field in `ProgramHeader`, cf. execute_task.cairo
+    let builtins_offset = 4;
+    let mut program_loader = ProgramLoader::new(&mut vm.segments.memory, builtins_offset);
+    let bootloader_version: BootloaderVersion = 0;
+    let loaded_program = program_loader
+        .load_program(&program_header_ptr, program, Some(bootloader_version))
+        .map_err(Into::<HintError>::into)?;
+
+    vm.segments.finalize(
+        Some(loaded_program.size),
+        program_data_base.segment_index as usize,
+        None,
+    );
+
+    exec_scopes.insert_value(vars::PROGRAM_ADDRESS, loaded_program.code_address);
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
-    use rstest::rstest;
+    use rstest::{fixture, rstest};
 
     use crate::hint_processor::builtin_hint_processor::hint_utils::get_ptr_from_var_name;
     use crate::types::relocatable::Relocatable;
@@ -75,6 +120,56 @@ mod tests {
                 segment_index: expected_program_data_segment_index as isize,
                 offset: 0
             }
+        );
+    }
+
+    #[fixture]
+    fn fibonacci() -> Program {
+        let program_content =
+            include_bytes!("../../../../../cairo_programs/fibonacci.json").to_vec();
+
+        Program::from_bytes(&program_content, Some("main"))
+            .expect("Loading example program failed unexpectedly")
+    }
+
+    #[rstest]
+    fn test_load_program(fibonacci: Program) {
+        let task = Task {
+            program: fibonacci.clone(),
+        };
+
+        let mut vm = vm!();
+        vm.run_context.fp = 1;
+        // Set program_header_ptr to (2, 0)
+        vm.segments = segments![((1, 0), (2, 0))];
+        let program_header_ptr = Relocatable::from((2, 0));
+        add_segments!(vm, 1);
+
+        let mut exec_scopes = ExecutionScopes::new();
+        exec_scopes.insert_value(vars::PROGRAM_DATA_BASE, program_header_ptr.clone());
+        exec_scopes.insert_value(vars::TASK, task);
+
+        let ids_data = ids_data!["program_header"];
+        let ap_tracking = ApTracking::new();
+
+        load_program_hint(&mut vm, &mut exec_scopes, &ids_data, &ap_tracking)
+            .expect("Hint failed unexpectedly");
+
+        // Note that we do not check the loaded content in memory here, this is tested
+        // in `program_loader.rs`
+
+        // The Fibonacci program has no builtins -> the header size is 4
+        let header_size = 4;
+        let expected_code_address = &program_header_ptr + header_size;
+
+        let program_address: Relocatable = exec_scopes.get(vars::PROGRAM_ADDRESS).unwrap();
+        assert_eq!(program_address, expected_code_address);
+
+        // Check that the segment was finalized
+        let expected_program_size = header_size + fibonacci.shared_program_data.data.len();
+        assert_eq!(
+            vm.segments.segment_sizes[&(program_address.segment_index as usize)],
+            expected_program_size
         );
     }
 }
---
+++ vm/src/hint_processor/builtin_hint_processor/bootloader/program_loader.rs
@@ -0,0 +1,347 @@
+use crate::hint_processor::builtin_hint_processor::bootloader::types::BootloaderVersion;
+use crate::serde::deserialize_program::BuiltinName;
+use crate::types::program::Program;
+use crate::types::relocatable::Relocatable;
+use crate::vm::errors::hint_errors::HintError;
+use crate::vm::errors::memory_errors::MemoryError;
+use crate::vm::vm_memory::memory::Memory;
+use crate::Felt252;
+
+#[derive(thiserror_no_std::Error, Debug)]
+pub enum ProgramLoaderError {
+    #[error(transparent)]
+    Memory(#[from] MemoryError),
+
+    #[error("Program has no entrypoint")]
+    NoEntrypoint,
+}
+
+impl Into<HintError> for ProgramLoaderError {
+    fn into(self) -> HintError {
+        match self {
+            ProgramLoaderError::Memory(e) => HintError::Memory(e),
+            _ => HintError::CustomHint(self.to_string().into_boxed_str()),
+        }
+    }
+}
+
+/// Creates an instance of `Felt252` from a builtin name.
+///
+/// Converts the builtin name to bytes then attempts to create a felt from
+/// these bytes. This function will fail if the builtin name is over 31 characters.
+///
+/// This is used by the loader to make the builtins used by the program to the Cairo
+/// code.
+fn builtin_to_felt(builtin: &BuiltinName) -> Result<Felt252, ProgramLoaderError> {
+    // The Python implementation uses the builtin name without suffix
+    let builtin_name = builtin
+        .name()
+        .strip_suffix("_builtin")
+        .unwrap_or(builtin.name());
+
+    // let buf = {
+    //     let mut padding: Vec<u8> = vec![0; 32 - builtin_name.len()];
+    //     padding.extend_from_slice(builtin_name.as_bytes());
+    //     padding
+    // };
+    let buf = builtin_name.as_bytes().clone();
+    Ok(Felt252::from_bytes_be_slice(&buf))
+}
+
+pub struct LoadedProgram {
+    /// Start of the program code in the VM memory.
+    pub code_address: Relocatable,
+    /// Total size of the program in memory, header included.
+    pub size: usize,
+}
+
+/// Loads a Cairo program in the VM memory.
+pub struct ProgramLoader<'vm> {
+    /// Memory accessor.
+    memory: &'vm mut Memory,
+    /// Offset of the builtin list array in the Cairo VM memory.
+    builtins_offset: usize,
+}
+
+impl<'vm> ProgramLoader<'vm> {
+    pub fn new(memory: &'vm mut Memory, builtins_offset: usize) -> Self {
+        Self {
+            memory,
+            builtins_offset,
+        }
+    }
+
+    fn load_builtins(
+        &mut self,
+        builtin_list_ptr: &Relocatable,
+        builtins: &[BuiltinName],
+    ) -> Result<(), ProgramLoaderError> {
+        for (index, builtin) in builtins.iter().enumerate() {
+            let builtin_felt = builtin_to_felt(&builtin)?;
+            self.memory
+                .insert_value(builtin_list_ptr + index, builtin_felt)?;
+        }
+
+        Ok(())
+    }
+
+    fn load_header(
+        &mut self,
+        base_address: &Relocatable,
+        program: &Program,
+        bootloader_version: Option<BootloaderVersion>,
+    ) -> Result<usize, ProgramLoaderError> {
+        // Map the header struct as memory addresses
+        let data_length_ptr = base_address.clone();
+        let bootloader_version_ptr = base_address + 1;
+        let program_main_ptr = base_address + 2;
+        let n_builtins_ptr = base_address + 3;
+        let builtin_list_ptr = base_address + 4;
+
+        let program_data = &program.shared_program_data.data;
+
+        let builtins = &program.builtins;
+        let n_builtins = builtins.len();
+        let header_size = self.builtins_offset + n_builtins;
+
+        // data_length does not include the data_length header field in the calculation.
+        let data_length = header_size - 1 + program_data.len();
+        let program_main = program
+            .shared_program_data
+            .main
+            .ok_or(ProgramLoaderError::NoEntrypoint)?;
+
+        let bootloader_version = bootloader_version.unwrap_or(0);
+
+        self.memory
+            .insert_value(data_length_ptr, data_length.clone())?;
+        self.memory.insert_value(
+            bootloader_version_ptr,
+            Felt252::from(bootloader_version.clone()),
+        )?;
+        self.memory
+            .insert_value(program_main_ptr, program_main.clone())?;
+        self.memory
+            .insert_value(n_builtins_ptr, n_builtins.clone())?;
+
+        self.load_builtins(&builtin_list_ptr, builtins)?;
+
+        Ok(header_size)
+    }
+
+    fn load_code(
+        &mut self,
+        base_address: &Relocatable,
+        program: &Program,
+    ) -> Result<(), ProgramLoaderError> {
+        for (index, opcode) in program.shared_program_data.data.iter().enumerate() {
+            self.memory.insert_value(base_address + index, opcode)?;
+        }
+
+        Ok(())
+    }
+
+    /// Loads a Cairo program in the VM memory.
+    ///
+    /// Programs are loaded in two parts:
+    /// 1. The program header contains metadata (ex: entrypoint, program size,
+    ///    builtins used by the program).
+    /// 2. The program itself.
+    ///
+    /// Starting from `base_address`, the header contains the following fields:
+    /// 1. The size of the header
+    /// 2. The bootloader version
+    /// 3. The program entrypoint
+    /// 4. The number of builtins used by the program
+    /// 5. The list of builtins used (converted to felts) as a C-style array.
+    ///
+    /// * `base_address`: Where to load the program, see above.
+    /// * `program`: The program to load.
+    /// * `bootloader_version`: The bootloader version. Defaults to 0.
+    ///
+    /// Returns the address where the code of the program is loaded and the program size.
+    pub fn load_program(
+        &mut self,
+        base_address: &Relocatable,
+        program: &Program,
+        bootloader_version: Option<BootloaderVersion>,
+    ) -> Result<LoadedProgram, ProgramLoaderError> {
+        let header_size = self.load_header(base_address, program, bootloader_version)?;
+
+        let program_address = base_address + header_size;
+        self.load_code(&program_address, program)?;
+
+        Ok(LoadedProgram {
+            code_address: program_address,
+            size: header_size + program.shared_program_data.data.len(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::vm::vm_memory::memory_segments::MemorySegmentManager;
+    use rstest::{fixture, rstest};
+
+    #[rstest]
+    #[case::output(BuiltinName::output, 122550255383924)]
+    #[case::pedersen(BuiltinName::pedersen, 8098989891770344814)]
+    fn test_builtin_to_felt(#[case] builtin_name: BuiltinName, #[case] expected_value: u64) {
+        let expected_felt = Felt252::from(expected_value);
+        let felt = builtin_to_felt(&builtin_name).unwrap();
+
+        assert_eq!(felt, expected_felt);
+    }
+
+    fn check_loaded_builtins(memory: &Memory, builtins: &[&str], builtin_list_ptr: Relocatable) {
+        let builtin_felts = memory
+            .get_integer_range(builtin_list_ptr, builtins.len())
+            .expect("Builtins not loaded properly in memory");
+        for (builtin, felt) in std::iter::zip(vec!["bitwise", "output", "pedersen"], builtin_felts)
+        {
+            let builtin_from_felt = String::from_utf8(felt.into_owned().to_bytes_be().to_vec())
+                .expect(
+                    format!(
+                        "Could not decode builtin from memory (expected {})",
+                        builtin
+                    )
+                    .as_ref(),
+                );
+            // Compare the last N characters, builtin_from_felt is padded left with zeroes
+            assert_eq!(&builtin_from_felt[32 - builtin.len()..32], builtin);
+        }
+    }
+
+    #[test]
+    fn test_load_builtins() {
+        let builtins = vec![
+            BuiltinName::bitwise,
+            BuiltinName::output,
+            BuiltinName::pedersen,
+        ];
+
+        let mut segments = MemorySegmentManager::new();
+        let builtin_list_ptr = segments.add();
+
+        let builtins_offset = 4;
+        let mut program_loader = ProgramLoader::new(&mut segments.memory, builtins_offset);
+
+        program_loader
+            .load_builtins(&builtin_list_ptr, &builtins)
+            .expect("Failed to load builtins in memory");
+
+        check_loaded_builtins(
+            &segments.memory,
+            &vec!["bitwise", "output", "pedersen"],
+            builtin_list_ptr,
+        );
+    }
+
+    #[fixture]
+    fn fibonacci() -> Program {
+        let program_content =
+            include_bytes!("../../../../../cairo_programs/fibonacci.json").to_vec();
+
+        Program::from_bytes(&program_content, Some("main"))
+            .expect("Loading example program failed unexpectedly")
+    }
+
+    fn check_loaded_header(
+        memory: &Memory,
+        header_address: Relocatable,
+        program: &Program,
+        bootloader_version: BootloaderVersion,
+    ) {
+        let header_felts = memory.get_integer_range(header_address, 4).unwrap();
+        let expected_data_length = program.shared_program_data.data.len() + 3;
+        let program_main = program.shared_program_data.main.unwrap();
+        let n_builtins = program.builtins.len();
+
+        assert_eq!(
+            header_felts[0].clone().into_owned(),
+            Felt252::from(expected_data_length)
+        );
+        assert_eq!(
+            header_felts[1].clone().into_owned(),
+            Felt252::from(bootloader_version)
+        );
+        assert_eq!(
+            header_felts[2].clone().into_owned(),
+            Felt252::from(program_main)
+        );
+        assert_eq!(
+            header_felts[3].clone().into_owned(),
+            Felt252::from(n_builtins)
+        );
+    }
+
+    #[rstest]
+    fn test_load_header(fibonacci: Program) {
+        let mut segments = MemorySegmentManager::new();
+        let base_address = segments.add();
+
+        let builtins_offset = 4;
+        let mut program_loader = ProgramLoader::new(&mut segments.memory, builtins_offset);
+
+        let bootloader_version: BootloaderVersion = 0;
+        program_loader
+            .load_header(&base_address, &fibonacci, Some(bootloader_version))
+            .expect("Failed to load program header in memory");
+
+        check_loaded_header(
+            &segments.memory,
+            base_address.clone(),
+            &fibonacci,
+            bootloader_version,
+        );
+
+        let builtin_list_ptr = &base_address + builtins_offset;
+        check_loaded_builtins(&segments.memory, &vec![], builtin_list_ptr);
+    }
+
+    fn check_loaded_program(memory: &Memory, code_address: Relocatable, program: &Program) {
+        let loaded_opcodes = memory
+            .get_continuous_range(code_address, program.shared_program_data.data.len())
+            .expect("Program not loaded properly in memory");
+
+        for (loaded, original) in std::iter::zip(loaded_opcodes, &program.shared_program_data.data)
+        {
+            assert_eq!(loaded, *original);
+        }
+    }
+
+    #[rstest]
+    fn test_load_program(fibonacci: Program) {
+        let mut segments = MemorySegmentManager::new();
+        let base_address = segments.add();
+
+        let builtins_offset = 4;
+        let mut program_loader = ProgramLoader::new(&mut segments.memory, builtins_offset);
+
+        let bootloader_version: BootloaderVersion = 0;
+        let loaded_program = program_loader
+            .load_program(&base_address, &fibonacci, Some(bootloader_version))
+            .expect("Failed to load program in memory");
+
+        let header_size = builtins_offset + fibonacci.builtins.len();
+        assert_eq!(loaded_program.code_address, &base_address + header_size);
+
+        assert_eq!(
+            loaded_program.size,
+            header_size + fibonacci.shared_program_data.data.len()
+        );
+
+        check_loaded_program(&segments.memory, loaded_program.code_address, &fibonacci);
+
+        check_loaded_header(
+            &segments.memory,
+            base_address.clone(),
+            &fibonacci,
+            bootloader_version,
+        );
+
+        let builtin_list_ptr = &base_address + builtins_offset;
+        check_loaded_builtins(&segments.memory, &vec![], builtin_list_ptr);
+    }
+}
